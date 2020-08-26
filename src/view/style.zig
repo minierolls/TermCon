3e8910efeaed706a27579e96190d0ab29201cf27
minierolls <minierolls@gmail.com>
2020-08-26 14:52:02 -0700

    <feat> Initial partial Style implementation

--- src/view/style.zig
+++ src/view/style.zig
@@ -3,7 +3,44 @@
 // SPDX-License-Identifier: MIT
 // This file is part of the `termcon` project under the MIT license.
 
-pub const Color = struct {};
+pub const ColorDefault = enum {
+    Highlight,
+    Background,
+    Foreground,
+};
+
+pub const ColorTrue; // TODO
+pub const ColorNamed; // TODO
+
+pub const Color24Bit = struct {
+    hex: u24,
+
+    pub fn initHex(hex: u24) CustomColor {
+        return CustomColor{ .hex = hex };
+    }
+
+    pub fn initRGB(red: u8, green: u8, blue: u8) CustomColor {
+        var hex: u32 = 0;
+        hex |= blue;
+        hex |= @as(u16, green) << 8;
+        hex |= @as(u24, red) << 16;
+        return CustomColor{ .hex = hex };
+    }
+};
+
+pub const ColorType = enum {
+    Default,
+    Custom,
+    True,
+    Named,
+};
+
+pub const Color = union(ColorType) {
+    Custom: Color24Bit,
+    True: ColorTrue,
+    Named: ColorNamed,
+    Default: ColorDefault,
+};
 
 pub const TextDecorations = struct {
     italic: bool,
